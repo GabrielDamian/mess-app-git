cabad05caf67ab83895d17809aef7ee3ae336b75
GabrielDamian <damiangabriel54@yahoo.com>
2021-09-21 19:12:53 +0300

    update

--- src/components/Dashboard/DashBlocked/DashBlocked.js
+++ src/components/Dashboard/DashBlocked/DashBlocked.js
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import './DashBlocked.css';
+import NotAllowedSVG  from '../../../images/notAllowed.svg';
+import { useHistory } from 'react-router';
 const DashBlocked = () => {
+    const history = useHistory();
+
     return (
         <div className="dash-blocked-container">
-            ss
+            <div className="dash-blocked-content">
+                <span>You are not allowed to view this page, please log in first</span>
+                <div className="submit-btn-signup">
+                    <button onClick={()=>history.push('/login')}>
+                        Login
+                    </button>
+                </div>
+                <img src={NotAllowedSVG} alt="not allowed svg"/>
+            </div>
+            
         </div>
     )
 }
--- src/components/NotFound404/NotFound404.js
+++ src/components/NotFound404/NotFound404.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import './NotFound404.css';
+import SvgNotFound from '../../images/404.svg';
 const NotFound404 = () => {
     return (
-        <div>
-            <p>404NotFound</p>
+        <div className="notfound-container">
+            <div className="notfound-container-content">
+                <span>Sorry, this page was not found...</span>
+                <img src={SvgNotFound} alt="svg not found"/>
+            </div>
         </div>
     )
 }
